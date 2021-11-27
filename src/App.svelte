6650b428096c6082a9c5b98e6d0da8e2014f1864
Alfredo Cruz <alfredox@gmail.com>
2021-11-27 08:08:47 -0600

    ✨  Adds TailwindCSS and changes styles
    Adds TailwindCSS library, based on this video
    (https://www.youtube.com/watch?v=gNXL8UQHDdA), and also on the
    installation for Vue 3 (https://tailwindcss.com/docs/guides/vue-3-vite).
    
    1. Installs TailwindCSS
    
    npm install -D tailwindcss@latest postcss@latest autoprefixer@latest
    npx tailwindcss init -p
    
    2. Adds the styles
    
    Add the TailwindCSS.svelte with global style, and reference it in the
    App.svelte.
    
    3. Style using TailwindCSS
    
    Removes the defined styles in the svelte components, and adds the
    styling classes to the svelte componenents: App and Counter.
    
    /Alfredo

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,26 @@
 <script lang="ts">
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
+  import TailwindCSS from './TailwindCSS.svelte';
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
+<TailwindCSS />
+
+<main class="text-center p-4 my-0 mx-auto">
+  <img src={logo} alt="Svelte Logo" class="inline-block w-64 h-64"/>
+  <h1 class="uppercase text-7xl text-red-600 font-thin my-8 mx-auto max-w-xs sm:max-w-none">Hello Typescript!</h1>
 
   <Counter />
 
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+  <p class="max-w-xs sm:max-w-none my-4 mx-auto leading-normal">
+    Visit <a class="text-blue-800" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
     apps.
   </p>
-  <p>
+  <p class="max-w-xs sm:max-w-none my-4 mx-auto leading-normal">
     This is an application test. Should review the package.json to see the things related to TS.
   </p>
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+  <p class="max-w-xs sm:max-w-none my-4 mx-auto leading-normal">
+    Check out <a class="text-blue-800" href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
     the officially supported framework, also powered by Vite!
   </p>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
